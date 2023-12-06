b0535a7d083a710978aeb40e53fc970f3bc153e5
biancaregulski <biancaregulski@yahoo.com>
2023-12-06 14:41:28 -0500

    Basic website outline

---
+++ src/components/home.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return(
+        <div className="main-content">
+            <p>Lorem ipsum</p>
+        </div>
+    );
+}
+
+export default Home;
---
+++ src/components/sidebar.tsx
@@ -0,0 +1,37 @@
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import Toggle from "react-toggle";
+import "react-toggle/style.css"
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { icon } from '@fortawesome/fontawesome-svg-core/import.macro'
+
+
+import girl_icon from '../girl_icon.jpeg';
+
+const Sidebar = () => {
+  return (
+    <>
+        <div className='sidebar'>
+            <div className='img-div'>
+                <img src={girl_icon} alt="Profile photograph" />
+            </div>
+            <h2>Bianca Regulski</h2>
+            
+            {/* <i className="fa-sharp fa-light fa-sun-bright"></i> */}
+
+            <div className="nav-link">
+                <Link to="/"> Home </Link>
+            </div>
+            <div className="nav-link">
+                <Link to="/about"> About </Link>
+            </div>
+            <div className="nav-link">
+                <Link to="/projects"> Projects </Link>
+            </div>
+        </div>
+    </>
+  );
+};
+
+export default Sidebar;
